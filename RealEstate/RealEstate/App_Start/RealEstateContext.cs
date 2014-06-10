6d4b4512c77673cf40829b48a66530e0a22af1d2
erdalkiran <erdalkiran@gmail.com>
2014-06-10 18:37:45 +0300

    Ch2

---
+++ RealEstate/RealEstate/App_Start/RealEstateContext.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="RealEstateContext.cs " company="Socar Turkey Petrol-Enerji Dağıtım A.Ş." owner="Erdal Dalkıran">
+// <createDate>10/06/2014 18:31</createDate>
+// </copyright>
+
+namespace RealEstate.App_Start
+{
+    using MongoDB.Driver;
+
+    using RealEstate.Properties;
+
+    public class RealEstateContext
+    {
+        #region Fields
+
+        public MongoDatabase Database;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        public RealEstateContext()
+        {
+            var client = new MongoClient(Settings.Default.RealEstate);
+            var server = client.GetServer();
+            Database = server.GetDatabase(Settings.Default.RealEstateDataBaseName);
+        }
+
+        #endregion
+    }
+}
---
+++ RealEstate/RealEstate/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿// <copyright file="HomeController.cs " company="Socar Turkey Petrol-Enerji Dağıtım A.Ş." owner="Erdal Dalkıran">
+// <createDate>10/06/2014 18:32</createDate>
+// </copyright>
+
+namespace RealEstate.Controllers
+{
+    using System.Web.Mvc;
+
+    using RealEstate.App_Start;
+
+    public class HomeController : Controller
+    {
+        #region Fields
+
+        public RealEstateContext Context = new RealEstateContext();
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        public ActionResult About()
+        {
+            Context.Database.GetStats();
+            return Json(Context.Database.Server.BuildInfo, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
